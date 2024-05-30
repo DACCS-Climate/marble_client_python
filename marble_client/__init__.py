c6f5645553ed14c7700a924790b01bb3a07fc1fd
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2024-05-30 16:01:21 -0400

    streamline code, deprecate some methods and make it generally more testable

--- marble_client/__init__.py
+++ marble_client/__init__.py
@@ -1,4 +1,4 @@
 from .client import MarbleClient
-from .exceptions import MarbleBaseError, ServiceNotAvailableError, UnknownNodeError
+from .exceptions import MarbleBaseError, ServiceNotAvailableError, UnknownNodeError, JupyterEnvironmentError
 from .node import MarbleNode
 from .services import MarbleService
